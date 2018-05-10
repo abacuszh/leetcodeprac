827aaa59b2244de199b637b3be8ac83fa9e95822
user <user@192.168.64.125>
2018-05-10 16:42:03 +0800

    add comment to the min depth method

--- leetcodeprac/src/main/java/leetcodeprac/Q1xx/Q11x/Q111MinTreeDepth.java
+++ leetcodeprac/src/main/java/leetcodeprac/Q1xx/Q11x/Q111MinTreeDepth.java
@@ -2,8 +2,20 @@ package leetcodeprac.Q1xx.Q11x;
 
 import leetcodeprac.utils.TreeNode;
 
+/**
+ * @author user
+ *
+ */
 public class Q111MinTreeDepth {
 
+	/**
+	 * find the min depth of a tree
+	 * 1	if the treenode is null, its depth is 0
+	 * 2	if one the of the branches is null, then calculate the min depth of the other one
+	 * 3	if both of the branches are not null, then find the min depth recursively
+	 * @param TreeNode
+	 * @return mindepth
+	 */
 	public int minDepth(TreeNode root) {
 
 		if ( null == root ) {
